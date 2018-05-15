16ce5b970bc87ac891363320fda910ca4b05e055
kasambuli <cynthiakasambuli@gmail.com>
2018-05-15 22:21:44 +0300

    add models to admin

--- instagram/instaclone/admin.py
+++ instagram/instaclone/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Image, Profile, Comments
 
-# Register your models here.
+#register models here
+admin.site.register(Image)
+admin.site.register(Profile)
+admin.site.register(Comments)
